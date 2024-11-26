ef84847228405a87ec09aa7e22cc0d082a06767b
Marvin <escorpiano18@bol.com.br>
2024-11-26 14:28:30 -0300

    ok

--- WEB/projeto_cad_usuarios/app_cad_usuarios/models.py
+++ WEB/projeto_cad_usuarios/app_cad_usuarios/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    # Dados Pessoais
+    nome = models.CharField(max_length=200)
+    idade = models.IntegerField()
+    data_nascimento = models.DateField()
+
+    # Dados Profissionais
+    ESCOLARIDADE_CHOICES = [
+        ('Ensino fundamental'),
+        ('Ensino Médio'),
+        ('Universitário'),
+        ('Formado'),
+    ]
+    escolaridade = models.CharField(max_length=20, choices=ESCOLARIDADE_CHOICES)
+    email = models.EmailField(max_length=255)
+    
+    EXPERIENCIA_CHOICES = [
+        ('Sim'),
+        ('Não'),
+    ]
+    experiencia_ti = models.CharField(max_length=3, choices=EXPERIENCIA_CHOICES)
+
+    # Disponibilidade
+    DISPONIBILIDADE_CHOICES = [
+        ('M'),
+        ('T'),
+        ('N'),
+    ]
+    disponibilidade = models.ManyToManyField('Disponibilidade', blank=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class Disponibilidade(models.Model):
+    valor = models.CharField(max_length=1, choices=Usuario.DISPONIBILIDADE_CHOICES)
+
+    def __str__(self):
+        return self.get_valor_display()
